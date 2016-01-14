dd423a381afd0f9aa3a2410bdc0db9c7d915f4f1
Konstantin Ladutenko <kostyfisik@gmail.com>
2016-01-14 17:25:46 +0300

    small change

--- merge-with-upstream.sh
+++ merge-with-upstream.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #git remote add upstream https://github.com/AndreyAkinshin/Russian-Phd-LaTeX-Dissertation-Template
+#git fetch origin --prune  # Delete multiple obsolete tracking branches
 git checkout master
 git pull
 git fetch upstream
